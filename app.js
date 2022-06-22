c5423c295c96863be5c10c5bf5158cc3be63d8ef
Realgreat01 <samsonrealgreat@gmail.com>
2022-06-22 23:54:38 +0100

    desktop UI

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-'use strict'
+
 const main = document.getElementById("main");
 
 const jobs = async () =>{
@@ -8,15 +8,23 @@ const jobs = async () =>{
 
 data.forEach(item=>{
   const sum =  (arr) =>{
-    console.table(item)
-  //  console.log(arr)
-    for(let i = 0; i<arr.length; i++){
-      const final = `${arr[i]}`
-     return final
-   }
+    const p = document.createElement("p");
+
+    if(arr.length>0){
+      for(let i = arr.length; i > 0 ; i--){
+        arr.forEach(element => {
+                  p.textContent += `<span style="margin: 5px; padding:10px" class="job-skill" id="skill">${element}</span>`
+                });
+
+        const final = `<span  id="skilled" >${p.textContent} </span>`
+        return final
+    }
+  }else{
+    return `
+    <p style="display:none ">None skill</p>`
   }
-
-
+}
+    
     main.innerHTML += `     
     <div id="details">
       
@@ -28,8 +36,8 @@ data.forEach(item=>{
   <div id="info">
     <div class="company">
         <h2>${item.company}</h2>
-        <h2 class="new">${item.new ?  "New !" : ""}</h2>
-        <h2 class="featured" id="featured" >${item.featured ?  "Featured" : ""}</h2>
+        <h2>${item.new ?  `<span class="new" >New!</span>` : `<span style="display:none;"></span>`}</h2>
+        <h2>${item.featured ?  `<span class="featured" " >Featured</span>` : `<span style="visibility:hidden">Featured</span>`}</h2>
     </div>
    <h2>${item.position}</h2>
     <div id="data">
@@ -42,10 +50,10 @@ data.forEach(item=>{
   </div>
 
   <div id="skills">
-  <p id="skill">${item.role}</p> 
-  <p id="skill">${item.level}</p> 
-    <p id="skill">${sum(item.languages)}</p> 
-    <p id="skill">${sum(item.tools)}</p> 
+  <p id="skill" style= "padding: 10px">${item.role}</p> 
+  <p id="skill" style= "padding: 10px">${item.level}</p> 
+  <p>${sum(item.tools)}</> 
+  <p>${sum(item.languages)}</p> 
   </div>
   
 </div>
@@ -54,5 +62,6 @@ data.forEach(item=>{
 }
 
 jobs()
+
 
 
